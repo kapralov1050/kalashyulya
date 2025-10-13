414f97563e98635e6cf1e6a5ff81efd0c6036518
Alexander Kapralov <pereulkovakapralov@gmail.com>
2025-10-13 22:59:54 +0300

    01-доработка vitest

--- vitest.config.ts
+++ vitest.config.ts
@@ -2,7 +2,7 @@ import { defineConfig } from 'vitest/config'
 
 export default defineConfig({
   test: {
-    environment: 'happydom', // или 'nuxt' если используете @nuxt/test-utils
+    environment: 'happy-dom', // или 'nuxt' если используете @nuxt/test-utils
     globals: true,
   },
 })
