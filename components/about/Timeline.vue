3f6546cbedcb199bb3e41f9e5e13e146eca84125
Alexander Kapralov <pereulkovakapralov@gmail.com>
2025-11-06 23:59:31 +0300

    правка таймлайна

--- components/about/Timeline.vue
+++ components/about/Timeline.vue
@@ -7,7 +7,8 @@
       fill="none"
       xmlns="http://www.w3.org/2000/svg"
       class="absolute top-10 left-1/2 transform -translate-x-1/2 z-[-1]
-        will-change-[stroke-dashoffset] scale-x-35 lg:scale-x-100 lg:block"
+        will-change-[stroke-dashoffset] scale-y-105 sm:scale-y-100 scale-x-35
+        lg:scale-x-100"
     >
       <path
         id="scrollPath"
