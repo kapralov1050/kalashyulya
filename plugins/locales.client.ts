04c1a709b5abfea8bfb5c533966b2df6c672c2d1
kapralov1050 <pereulkovakapralov@gmail.com>
2026-04-29 14:44:28 +0300

    обновление локалей

--- plugins/locales.client.ts
+++ plugins/locales.client.ts
@@ -1,8 +1,4 @@
 export default defineNuxtPlugin(async () => {
   const { fetchLocales } = useLocalesStore()
-  const { locales } = storeToRefs(useLocalesStore())
-
-  if (!locales.value) {
-    await fetchLocales()
-  }
+  await fetchLocales()
 })
